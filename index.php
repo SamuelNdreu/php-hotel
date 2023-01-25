3b7f565be64a6157fd4d5c5879bb4a9e6ba0769a
SamuelNdreu <samwellsoficcial@gmail.com>
2023-01-25 19:02:31 +0100

    fixed table

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <title>Hotel Table Php</title>
 </head>
 
 <body>
@@ -50,13 +51,34 @@
 
     ];
 
-    foreach ($hotels as $key => $hotel) {
-        foreach ($hotel as $key => $value) {
-            echo "<p>{$key}: {$value}</p>";
-        }
-    }
 
     ?>
+    <div class="container pt-5">
+        <table class="table table-primary">
+            <thead>
+                <tr>
+                    <th scope='col' class="bg-info">Nome</th>
+                    <th scope='col'class="bg-info">Descrizione</th>
+                    <th scope='col'class="bg-info">Parcheggio</th>
+                    <th scope='col'class="bg-info">Voto</th>
+                    <th scope='col'class="bg-info">Distanza dal centro</th>
+                </tr>
+            </thead>
+            <?php
+            echo "<tbody>";
+            foreach ($hotels as $hotel) {
+                echo "<tr>";
+                echo "<td>" . $hotel['name'] . "</td>";
+                echo "<td>" . $hotel['description'] . "</td>";
+                echo "<td>" . $hotel['parking'] . "</td>";
+                echo "<td>" . $hotel['vote'] . "</td>";
+                echo "<td>" . $hotel['distance_to_center'] . "</td>";
+                echo "</tr>";
+            }
+            echo "</tbody>";
+            ?>
+        </table>
+    </div>
 </body>
 
 </html>
