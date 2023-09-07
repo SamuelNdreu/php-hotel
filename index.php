aad290701a1a9f7231935811c6ebb1f0f13863f6
SamuelNdreu <samwellsoficcial@gmail.com>
2023-09-07 13:05:14 +0200

    created header with html structure

--- index.php
+++ index.php
@@ -5,7 +5,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--CSS-->
+    <link rel="stylesheet" href="./styles/style.css">
+
+    <!--Bootstrap-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
     <title>Hotel Table Php</title>
 </head>
 
@@ -51,35 +56,38 @@ $hotels = [
 
 ];
 
+                    //foreach($hotels as $key => $hotel){
+                    //    echo "<div class='col-5 border border-secondary g-3 me-autod '>";
+                    //    echo "<p>Nome: {$hotel['name']} </p>";
+                     //   echo "<p>Descrizione: {$hotel['description']} </p>";
+                    //    echo "<p>Parcheggio: {$hotel['parking']} </p>";
+                    //    echo "<p>Voto: {$hotel['vote']} </p>";
+                    //    echo "<p>Distanza dal centro: {$hotel['distance_to_center']}mt </p>";
+                    //    echo "</div>";
+                   // }
+
+
 
 ?>
+    <header>
+        <h1 class="text-center">
+            Hotels
+        </h1>
 
-<div class="container pt-5">
-        <table class="table table-primary">
-            <thead>
-                <tr>
-                    <th scope='col' class="bg-info">Nome</th>
-                    <th scope='col'class="bg-info">Descrizione</th>
-                    <th scope='col'class="bg-info">Parcheggio</th>
-                    <th scope='col'class="bg-info">Voto</th>
-                    <th scope='col'class="bg-info">Distanza dal centro</th>
-                </tr>
-            </thead>
-            <?php
-            echo "<tbody>";
-            foreach ($hotels as $hotel) {
-                echo "<tr>";
-                echo "<td>" . $hotel['name'] . "</td>";
-                echo "<td>" . $hotel['description'] . "</td>";
-                echo "<td>" . $hotel['parking'] . "</td>";
-                echo "<td>" . $hotel['vote'] . "</td>";
-                echo "<td>" . $hotel['distance_to_center'] . "</td>";
-                echo "</tr>";
-            }
-            echo "</tbody>";
-            ?>
-        </table>
-    </div>
+        <hr>
+    </header>
+        <!-- 
+            $hotels = 
+            [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ]
+        -->
+
+
 </body>
 
 </html>
